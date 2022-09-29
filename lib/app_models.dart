9c447594cb1236fe21ebfd4551922123db2b01f6
Chasseur Agace <34769013+chasseuragace@users.noreply.github.com>
2022-09-29 14:43:20 +0545

    Update app_models.dart

--- lib/app_models.dart
+++ lib/app_models.dart
@@ -3,3 +3,4 @@ library app_models;
 export 'package:app_models/models/models/user.dart' show User;
 export 'package:app_models/models/models/items/items.dart';
 export 'package:app_models/models/models/user.dart';
+export 'package:app_models/models/models/items/image.dart';
